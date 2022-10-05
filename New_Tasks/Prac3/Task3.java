6131dd9d05076f5a406d04447f5394fc89d93ce1
Gleb Nechaev <nechaevgleb@inbox.ru>
2022-10-05 15:14:58 +0300

    prac3 added

---
+++ New_Tasks/Prac3/Task3.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+public class Task3 {
+    public static void main(String[] args) {
+
+        final Random rg = new Random();
+
+        int[] arr;
+        arr = new int[4];
+
+        for (int i = 0; i < 4; i++) {
+            arr[i] = rg.nextInt() % 90 + 10;
+            System.out.printf("%d ", arr[i]);
+        }
+
+        int prev = arr[0];
+        for (int i = 1; i < 4; i++) {
+            if (prev <= arr[i]) {
+                System.out.println("\nМассив НЕ строго возрастающий!");
+                return;
+            }
+            prev = arr[i];
+        }
+        System.out.println("\nМассив строго возрастает!");
+    }
+}
--- Task3/Task3.java
+++ Task3/Task3.java
@@ -1,3 +1,4 @@
+
 // Генерирует 10 эл-ов массива double двумя способами:
 // 1. util.Random 
 // 2. Math.random
@@ -7,21 +8,18 @@ import java.util.Random;
 import java.util.List;
 import java.util.ArrayList;
 
-
 public class Task3 {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         List<Double> MathRArray = new ArrayList<>();
         List<Double> UtilRArray = new ArrayList<>();
 
         final Random rg = new Random();
 
-
         for (int i = 0; i < 10; i++) {
-            MathRArray.add( Math.random());
+            MathRArray.add(Math.random());
             UtilRArray.add(rg.nextDouble());
         }
-
 
         System.out.print("Math.Random Array:");
         for (int i = 0; i < 10; i++) {
@@ -33,10 +31,8 @@ public class Task3 {
             System.out.printf("%f ", UtilRArray.get(i));
         }
 
-
         MathRArray.sort(Comparator.naturalOrder());
         UtilRArray.sort(Comparator.naturalOrder());
-
 
         System.out.print("\n\nMath.Random Array SORTED:");
         for (int i = 0; i < 10; i++) {
